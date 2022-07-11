d7cc6eb849c4b42f9888e62052a5f65edb63210d
wanxinghua <wanxinghua@tju.edu.cn>
2022-07-11 22:39:14 +0800

    增加core模块

---
+++ rpc-core/src/main/java/cn/rpc/core/balancer/LoadBalancer.java
@@ -0,0 +1,10 @@
+package cn.rpc.core.balancer;
+
+/**
+ * @program: rpc-wanxh
+ * @Date: 2022/7/11 22:38
+ * @Author: 阿左不是蜗牛
+ * @Description: 负载均衡
+ */
+public interface LoadBalancer {
+}
---
+++ rpc-core/src/main/java/cn/rpc/core/register/RegistryService.java
@@ -0,0 +1,10 @@
+package cn.rpc.core.register;
+
+/**
+ * @program: rpc-wanxh
+ * @Date: 2022/7/11 22:37
+ * @Author: 阿左不是蜗牛
+ * @Description: 服务注册
+ */
+public interface RegistryService {
+}
